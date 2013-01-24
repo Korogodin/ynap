995229dc7c6bc1e753cf4000d76986d3f77bf3ad
korogodiniv@gmail.com <korogodiniv@gmail.com@dc8c7f59-bd97-ec13-8752-b8f4ac7b5fd7>
2013-01-24 20:49:05 +0000

    Реализовано интегральное решение уравнений Стратоновича для двумерной задачи

---
+++ trunk/main.m
@@ -0,0 +1,23 @@
+try 
+    close(MW.handle); % Close old output form
+end
+
+close all
+clear 
+clc
+
+globals;
+
+addpath([pwd '/callback']); % Callbacks for Control Panel
+addpath([pwd '/basic-interface']); % Functions for base interface
+addpath([pwd '/interface']); % Functions for interface
+addpath([pwd '/stratonovich']); % Class of integral solution of Stratonovich equation
+
+MW = CMainWindow('Multiantennas receiver');
+if MW.handle == 0
+    clear MW;
+    return;
+end
+
+interface;
+
